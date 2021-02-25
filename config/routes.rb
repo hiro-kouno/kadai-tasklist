b04b8804645df54ebb25c6688fa4e4d7320a553f
hiro.kouno <k.hiro430@gmail.com>
2021-02-25 14:53:37 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  
   root to: 'tasks#index'
-   
-  resources :tasks
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -18,7 +7,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:create]
   
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks
 end
