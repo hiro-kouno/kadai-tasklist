46e94df8ebea944a0f47c457d7c1898811029e5a
hiro.kouno <k.hiro430@gmail.com>
2021-02-26 07:51:30 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
-      @tasks = current_user.tasks.order(id: :desc)
-    end
+    @tasks = current_user.tasks.order(id: :desc)
   end
 
   def show
@@ -16,7 +14,6 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
 
     if @task.save
